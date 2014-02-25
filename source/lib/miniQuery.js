20f50aba8d0005bed9e9cf7c578ba0e0c4dd2dea
& <pair+bigspencey.dslachtman@devbootcamp.com>
2014-02-25 10:47:56 -0800

    SweetSelector selecting all the things

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -1,3 +1,15 @@
-/*!
- * minQuery
- */
+var SweetSelector = {
+  select: function(tag) {
+    if (/#/.test(tag)) {
+      tag = tag.substr(1)
+      return document.getElementById(tag);
+    } else if (/\./.test(tag)) {
+      tag = tag.substr(1)
+      return document.getElementsByClassName(tag);
+    } else {
+      return document.getElementsByTagName(tag);
+    }
+  }
+}
+
+// sweetSelector.select('#eyed');

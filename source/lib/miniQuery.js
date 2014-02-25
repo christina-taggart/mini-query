8f88debdf2121719d158a4802cc31e00de024d22
Emily Foley <foleyemilym@gmail.com>
2014-02-25 12:12:50 -0800

    add SweetSelector, fully functional element selector

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -1,3 +1,22 @@
-/*!
- * minQuery
- */
+var sweetSelector = (function() {
+
+  return {
+    select: function(thingToSelect)
+    {
+      if (thingToSelect.match(/#/)) {
+        thingToSelect = thingToSelect.replace(/#/, "")
+        return document.getElementById(thingToSelect);
+        }
+      else if (thingToSelect.match(/\./))
+        {
+        thingToSelect = thingToSelect.replace(/\./, "")
+        return document.getElementsByClassName(thingToSelect);
+        }
+      else
+        {
+        return document.getElementsByTagName(thingToSelect);
+        }
+      }
+    }
+  })();
+

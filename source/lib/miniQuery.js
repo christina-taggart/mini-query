6a5c2f5566a56789198982a5a483fb2435c4e663
& <pair+dgoodman1224.mh120888@devbootcamp.com>
2014-02-25 10:33:49 -0800

    release 1

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -3,4 +3,18 @@ SweetSelector = {
     return document.querySelectorAll(element);
   }
 }
+
+DOM = {
+  hide: function(element) {
+  var thing = SweetSelector.select(element)[0]
+  thing.style.visibility='hidden';
+  },
+
+  show: function(element) {
+    var thing = SweetSelector.select(element)[0]
+    thing.style.visibility = 'visible'
+  }
+}
+
 SweetSelector
+DOM

28b7363302655ea6beecc6aa32975872d0bb7877
& <pair+christina-taggart.qsymmachus@devbootcamp.com>
2014-02-25 10:23:14 -0800

    Adds SweetSelector module
    Module has #select method to select DOM elements by id, class, or tag name.

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -1,3 +1,19 @@
 /*!
  * minQuery
  */
+
+/* SweetSelector module: select elements within the DOM! */
+var SweetSelector = {
+  select: function(selector) {
+    var choppedSelector = selector.slice(1, selector.length)
+    if (selector[0] === "#") {
+      return document.getElementById(choppedSelector);
+    }
+    else if (selector[0] === ".") {
+      return document.getElementsByClassName(choppedSelector);
+    }
+    else {
+      return document.getElementsByTagName(selector);
+    }
+  }
+}

393f5ededdd7657e0fa57737def4f24c507e2890
Ricardo Rubio <rickarubio@gmail.com>
2014-02-25 11:12:56 -0800

    Release 0: selector library functionality implemented

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -4,8 +4,23 @@ var SweetSelector = (function() {
   return {
     // public variables and functions
     myPublicVar: "foo",
-    myPublicFunc: function(){ 
-      console.log("test")
+    select: function(target) { 
+      if (typeof(target) != "string") {
+        throw "select() only works with strings"
+      }
+
+      if (target.charAt(0) == "#") {
+        target = target.replace("#", "");
+        return document.getElementById(target);
+      } else if (target.charAt(0) == ".") {
+        target = target.slice(1)
+        target = target.replace(".", " ")
+        return document.getElementsByClassName(target);
+      } else {
+        return document.getElementsByTagName(target);
+      }
+
+      console.log(target); // remove me
     }
   };
 })();

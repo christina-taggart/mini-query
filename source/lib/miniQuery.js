641583322678c14cadcdb5789435eb3bd53c2841
& <pair+christina-taggart.qsymmachus@devbootcamp.com>
2014-02-25 10:52:04 -0800

    Add DOM module that hides and shows elements, removes and adds Class names

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -10,10 +10,34 @@ var SweetSelector = {
       return document.getElementById(choppedSelector);
     }
     else if (selector[0] === ".") {
-      return document.getElementsByClassName(choppedSelector);
+      return document.getElementsByClassName(choppedSelector)[0];
     }
     else {
-      return document.getElementsByTagName(selector);
+      return document.getElementsByTagName(selector)[0];
+    }
+  }
+}
+
+/* DOM Module: hide and show elements, add and remove Class names */
+
+var DOM = {
+  hide: function(selector){
+    element = SweetSelector.select(selector);
+    element.style.visibility='hidden';
+  },
+
+  show: function(selector){
+    element = SweetSelector.select(selector);
+    element.style.visibility='visible';
+  },
+
+  addClass: function(selector, className){
+    element = SweetSelector.select(selector);
+    if (element.className) {
+      element.className += " " + className;
+    }
+    else {
+      element.className = className;
     }
   }
 }

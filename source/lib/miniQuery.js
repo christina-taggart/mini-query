f6444358e34b40da5d48d76eb82aba5dd6ba4fa6
& <pair+amahesh0501.mechamanny@devbootcamp.com>
2014-02-25 10:33:14 -0800

    added select functionality for sweet selectors

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -1,3 +1,19 @@
 /*!
  * minQuery
  */
+
+ var sweetSelector = {
+
+  select: function(searchTerm) {
+    if (searchTerm[0] === "#") {
+     return document.getElementById(searchTerm.slice(1, searchTerm.length+1))
+    }
+    else if (searchTerm[0] === ".") {
+     return document.getElementsByClassName(searchTerm.slice(1, searchTerm.length+1));
+    }
+    else {
+      return document.getElementsByTagName(searchTerm)
+    }
+  }
+ }
+

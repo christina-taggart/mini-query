11592ce62738bb6b2b1b08a176fb6d64b4d0a395
& <pair+amahesh0501.mechamanny@devbootcamp.com>
2014-02-25 11:29:23 -0800

    implemented dom manipulation hide and show elements

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -2,9 +2,56 @@
  * minQuery
  */
 
- var sweetSelector = {
+var DOM = {
 
-  select: function(searchTerm) {
+   hide: function(searchTerm) {
+
+    var makeHidden = function(selector) {
+      selector.style.display="none"
+    }
+
+    if (searchTerm[0] === "#") {
+
+     return document.getElementById(searchTerm.slice(1, searchTerm.length+1)).style.display="none"
+    }
+
+    else if (searchTerm[0] === ".") {
+     var hideClassNodeList =  document.getElementsByClassName(searchTerm.slice(1, searchTerm.length+1))
+     var hideClassNodeArray = [].slice.call(hideClassNodeList).map(makeHidden);
+    }
+
+    else {
+      var hideTagNodeList =   document.getElementsByTagName(searchTerm)
+      var hideTagNodeArray = [].slice.call(hideTagNodeList).map(makeHidden);
+    }
+  },
+
+
+
+   show: function(searchTerm) {
+
+    var makeVisible = function(selector) {
+      selector.style.display="block"
+    }
+
+    if (searchTerm[0] === "#") {
+     return document.getElementById(searchTerm.slice(1, searchTerm.length+1)).style.display="block"
+    }
+    else if (searchTerm[0] === ".") {
+     var showClassNodeList =  document.getElementsByClassName(searchTerm.slice(1, searchTerm.length+1))
+     var showClassNodeArray = [].slice.call(showClassNodeList).map(makeVisible);
+    }
+    else {
+     var showTagNodeList =  document.getElementsByTagName(searchTerm)
+     var showTagNodeArray = [].slice.call(showTagNodeList).map(makeVisible);
+
+    }
+  }
+}
+
+
+var sweetSelector = (function(){
+  var select = function(searchTerm) {
     if (searchTerm[0] === "#") {
      return document.getElementById(searchTerm.slice(1, searchTerm.length+1))
     }
@@ -15,5 +62,9 @@
       return document.getElementsByTagName(searchTerm)
     }
   }
- }
+  return {
+    select: select
+  }
+}())
+
 

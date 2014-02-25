460d93e204db13928ab53cbd83da75c635441eff
Ricardo Rubio <rickarubio@gmail.com>
2014-02-25 15:12:18 -0800

    Release 1: DOM manipulation functionality completed

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -1,9 +1,7 @@
 var SweetSelector = (function() {
   // private variables and functions
-  var myPrivateVar, myPrivateMethod;
   return {
     // public variables and functions
-    myPublicVar: "foo",
     select: function(target) { 
       if (typeof(target) != "string") {
         throw "select() only works with strings"
@@ -20,7 +18,50 @@ var SweetSelector = (function() {
         return document.getElementsByTagName(target);
       }
 
-      console.log(target); // remove me
+    }
+  };
+})();
+
+var DOM = (function(){
+  // private vars and funcs
+  var wrapElementWithArray = function(element) {
+    if (element.toString() != "[object NodeList]") {
+      return [element];
+    } else {
+      return element;
+    }
+  }
+
+  return {
+  // public vars and funcs
+    hide: function(target){
+      target = SweetSelector.select(target)
+      target = wrapElementWithArray(target)
+      for(i=0; i < target.length; i++){
+        target[i].style.display = "none";
+      }
+    },
+    show: function(target) {
+      target = SweetSelector.select(target)
+      target = wrapElementWithArray(target)
+      for(i=0; i < target.length; i++) {
+        target[i].style.display = "";
+      }
+    },
+    // ex: DOM.addClass('.klass', 'improved-klass')
+    addClass: function(target, nameOfClass) {
+      target = SweetSelector.select(target)
+      target = wrapElementWithArray(target)
+      for(i=0; i < target.length; i++) {
+        target[i].className = target[i].className + " " + nameOfClass;
+      }
+    },
+    removeClass: function(target, nameOfClass) {
+      target = SweetSelector.select(target)
+      target = wrapElementWithArray(target)
+      for(i=0; i < target.length; i++) {
+        target[i].className = target[i].className.replace(nameOfClass, "")
+      }
     }
   };
 })();

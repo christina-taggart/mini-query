ea178770946da103989071d3e9c7a6d99fec9b2f
& <pair+kyeh.leishman@devbootcamp.com>
2014-02-25 10:25:18 -0800

    Added code for miniQuery selector

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -1,3 +1,38 @@
 /*!
  * minQuery
  */
+
+var SweetSelector = {
+  select: function(query) {
+    var first_char = query[0];
+
+    switch(first_char){
+      case '#':
+        return this.selectId(query.substr(1));
+        break;
+      case '.':
+        return this.selectClass(query.substr(1));
+        break;
+      default:
+        return this.selectElement(query);
+    }
+
+
+  },
+
+  selectId: function(id_name) {
+    console.log("selectId");
+    return document.getElementById(id_name);
+  },
+
+  selectClass: function(class_name) {
+    console.log("selectClass");
+    return document.getElementsByClassName(class_name);
+  },
+
+  selectElement: function(element_name) {
+    console.log("selectElement");
+    return document.getElementsByTagName(element_name);
+  }
+
+}

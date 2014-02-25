1049a9842bfa48f3898c8dec3b61ac29aa58b81d
Ricardo Rubio <rickarubio@gmail.com>
2014-02-25 10:38:38 -0800

    setup skeleton of miniQuery using Module pattern to build upon

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -1,3 +1,11 @@
-/*!
- * minQuery
- */
+var SweetSelector = (function() {
+  // private variables and functions
+  var myPrivateVar, myPrivateMethod;
+  return {
+    // public variables and functions
+    myPublicVar: "foo",
+    myPublicFunc: function(){ 
+      console.log("test")
+    }
+  };
+})();

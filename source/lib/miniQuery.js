c4261b999ba9516a105a9122955e648ae721b484
Apprentice <apprentice@DBC-Infinity-2.local>
2014-02-25 10:31:24 -0800

    release 1: Dom manipulation

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -4,5 +4,23 @@
 var SweetSelector = {
   select: function(query) {
     return document.querySelector(query)
+  },
+
+  hide: function(query) {
+    this.select(query).style.display = 'none'
+  },
+
+  show: function(query) {
+    this.select(query).style.display = ''
+  },
+
+  addClass: function(query, klass) {
+    this.select(query).className += ' ' + klass
+  },
+
+  removeClass: function(query, klass) {
+    var newClassName = this.select(query).className.replace(' ' + klass, '');
+    this.select(query).className = newClassName;
   }
+
 };

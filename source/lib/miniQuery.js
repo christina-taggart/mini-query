e43d7b374b0c76cdb870b05e17815bd78412f57e
Apprentice <apprentice@DBC-Infinity-2.local>
2014-02-25 11:38:23 -0800

    Release 2: Event Dispatch

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -1,26 +1,57 @@
 /*!
  * minQuery
  */
+
 var SweetSelector = {
   select: function(query) {
-    return document.querySelector(query)
-  },
+    return document.querySelectorAll(query);
+  }
+};
 
+var Dom = {
   hide: function(query) {
-    this.select(query).style.display = 'none'
+    var elements = SweetSelector.select(query);
+    for(var i = 0; i < elements.length; i++) {
+      elements[i].style.display = 'none';
+    }
   },
 
   show: function(query) {
-    this.select(query).style.display = ''
+    var elements = SweetSelector.select(query);
+    for(var i = 0; i < elements.length; i++) {
+      elements[i].style.display = '';
+    }
   },
 
   addClass: function(query, klass) {
-    this.select(query).className += ' ' + klass
+    var elements = SweetSelector.select(query);
+    for(var i = 0; i < elements.length; i++) {
+      elements[i].className += ' ' + klass;
+    }
   },
 
   removeClass: function(query, klass) {
-    var newClassName = this.select(query).className.replace(' ' + klass, '');
-    this.select(query).className = newClassName;
+    var elements = SweetSelector.select(query);
+    for(var i = 0; i < elements.length; i++) {
+      var newClassName = elements[i].className.replace(klass, '');
+      elements[i].className = newClassName;
+    }
+  }
+}
+
+var EventDispatcher = {
+  on: function(observer, event, callback) {
+    var elements = SweetSelector.select(observer);
+    for(var i = 0; i < elements.length; i++) {
+      elements[i][event] = callback;
+    }
+  },
+
+  trigger: function(observer, event) {
+    var elements = SweetSelector.select(observer);
+    for(var i = 0; i < elements.length; i++) {
+      elements[i][event].call();
+    }
   }
 
-};
+}

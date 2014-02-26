3a5cb1642d252a0ff53dbff1423caf0ba9759e1c
& <pair+dgoodman1224.mh120888@devbootcamp.com>
2014-02-25 21:51:52 -0800

    redid event functionality

--- source/lib/miniQuery.js
+++ source/lib/miniQuery.js
@@ -1,54 +1,55 @@
-SweetSelector = {
-  select: function(element) {
-    return document.querySelectorAll(element);
-  }
+miniQuery = {
+
+  SweetSelector: {
+    select: function(element) {
+      return document.querySelectorAll(element);
+    }
+  },
+
+  DOM: (function() {
+    var _desiredElement = function(element) {
+      return miniQuery.SweetSelector.select(element)[0];
+    }
+    return{
+      hide: function(element) {
+        _desiredElement(element).style.visibility='hidden';
+      },
+      show: function(element) {
+        _desiredElement(element).style.visibility = 'visible'
+      },
+      addClass: function(element, className) {
+        _desiredElement(element).classList.add(className)
+      },
+      removeClass: function(element, className) {
+        _desiredElement(element).classList.remove(className)
+      }
+    }
+  }()),
+
+  EventDisptacher: (function() {
+    var _desiredElement = function(element) {
+      return miniQuery.SweetSelector.select(element)[0];
+    }
+    return {
+      on: function(element, customEvent, toDoFunction){
+        _desiredElement(element)[customEvent] = toDoFunction;
+        _desiredElement(element).addEventListener(customEvent, toDoFunction, false);
+      },
+      trigger: function(element, customEvent) {
+        _desiredElement(element)[customEvent]()
+      }
+    }
+  }()),
+
+  Jaxinator: (function() {
+    return {
+    request: function(parameters) {
+      var ourJax = new XMLHttpRequest();
+      ourJax.onload = parameters['success']
+      ourJax.onerror = parameters['failure']
+      ourJax.open(parameters['method'], parameters['url'])
+      ourJax.send()
+    }
+  }})()
+
 }
-
-var DOM = (function(){
-  var _desiredElement = function(element) {
-    return SweetSelector.select(element)[0];
-  }
-  return{
-    hide: function(element) {
-      _desiredElement(element).style.visibility='hidden';
-    },
-    show: function(element) {
-      _desiredElement(element).style.visibility = 'visible'
-    },
-    addClass: function(element, className) {
-      _desiredElement(element).classList.add(className)
-    },
-    removeClass: function(element, className) {
-      _desiredElement(element).classList.remove(className)
-    }
-  }
-}())
-
-EventDisptacher = (function() {
-  var _desiredElement = function(element) {
-    return SweetSelector.select(element)[0];
-  }
-  return {
-    on: function(element, customEvent, toDoFunction){
-      _desiredElement(element).addEventListener(customEvent, toDoFunction, false)
-    },
-    trigger: function(element, customEvent) {
-      _desiredElement(element).dispatchEvent(customEvent)
-    }
-  }
-}())
-
-Jaxinator = (function() {
-  return {
-  request: function(parameters) {
-    var ourJax = new XMLHttpRequest();
-    ourJax.onload = parameters['success']
-    ourJax.onerror = parameters['failure']
-    ourJax.open(parameters['method'], parameters['url'])
-    ourJax.send()
-  }
-}})()
-
-SweetSelector
-DOM
-EventDisptacher
